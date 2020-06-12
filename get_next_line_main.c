7e2e39ffdd5980adf7e90e5e07dd2761f808168a
unknown <anachristofis@gmail.com>
2020-06-12 16:46:58 -0300

    Add main

---
+++ get_next_line_main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main ()
+{
+	int fd;
+	int fd2;
+	char *line;
+
+	line = (char*)malloc(100*sizeof(char*));
+	fd = open ("texto", O_RDONLY);
+	fd2 = open ("texto2", O_RDONLY);
+	printf("retorno %d\n", get_next_line(fd, &line));
+	printf("linha %s\n", line);
+	printf("\nsegunda rodada\n");
+	printf("retorno %d\n", get_next_line(fd, &line));
+	printf("linha %s\n", line);
+	printf("\nterceira rodada\n");
+	printf("retorno %d\n", get_next_line(fd2, &line));
+	printf("linha %s\n", line);
+	printf("\nquarta rodada\n");
+	printf("retorno %d\n", get_next_line(fd, &line));
+	printf("linha %s\n", line);
+	printf("\nquinta rodada\n");
+	printf("retorno %d\n", get_next_line(fd, &line));
+	printf("linha %s\n", line);
+	printf("\nsexta rodada\n");
+	printf("retorno %d\n", get_next_line(fd2, &line));
+	printf("linha %s\n", line);
+	return (0);
+}
