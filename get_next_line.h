81e63015d5b56363e97bc2c76a6a4539f63322b5
Ana Christofis <achristo@c1r1p4.42sp.org.br>
2020-07-05 10:24:44 -0300

    Review8-Norminette ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: achristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 16:23:36 by achristo          #+#    #+#             */
-/*   Updated: 2020/02/16 17:13:01 by achristo         ###   ########.fr       */
+/*   Updated: 2020/07/05 10:22:54 by achristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int	get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
