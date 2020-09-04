e0e08ec9be50ba9b09e6a86fd3e256916dcbae89
Ana Christofis <achristo@c1r2p2.42sp.org.br>
2020-09-04 17:09:53 -0300

    Free buf @ end of file

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,14 +6,14 @@
 /*   By: achristo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/16 16:08:22 by achristo          #+#    #+#             */
-/*   Updated: 2020/08/16 11:29:33 by achristo         ###   ########.fr       */
+/*   Updated: 2020/09/04 17:01:45 by achristo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 #include <limits.h>
 
-int	ft_isline(char *s)
+int		ft_isline(char *s)
 {
 	size_t	i;
 	size_t	len;
@@ -29,7 +29,14 @@ int	ft_isline(char *s)
 	return (1);
 }
 
-int	ft_readfile(int fd, int count, char **buf)
+char	*ft_free(char *s)
+{
+	free(s);
+	s = NULL;
+	return (s);
+}
+
+int		ft_readfile(int fd, int i, char **buf)
 {
 	int		r;
 	char	*new;
@@ -38,43 +45,46 @@ int	ft_readfile(int fd, int count, char **buf)
 	if ((new = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))) == NULL)
 		return (-1);
 	if ((r = read(fd, new, BUFFER_SIZE)) < 0)
+	{
+		new = ft_free(new);
 		return (-1);
+	}
 	new[r] = '\0';
-	aux = *buf;
-	*buf = ft_strjoin(*buf, new);
-	free(aux);
-	count = ft_isline(*buf);
+	aux = ft_strjoin(*buf, new);
+	*buf = ft_free(*buf);
+	*buf = aux;
+	i = ft_isline(*buf);
 	if (new[0] == '\0')
-		count = 0;
-	free(new);
-	return (count);
+		i = 0;
+	new = ft_free(new);
+	return (i);
 }
 
-int	get_next_line(int fd, char **line)
+int		get_next_line(int fd, char **line)
 {
-	int			count;
 	static char	*buf[OPEN_MAX];
 	int			i;
 	char		*aux;
 
-	count = 1;
-	if (fd < 0 || BUFFER_SIZE <= 0 || line == NULL || (i = 0) != 0)
+	i = 1;
+	if (fd < 0 || BUFFER_SIZE <= 0 || line == NULL)
 		return (-1);
 	if (buf[fd] == NULL)
 		if ((buf[fd] = ft_strdup("")) == NULL)
 			return (-1);
-	while (count == 1)
-		if ((count = ft_readfile(fd, count, &buf[fd])) == -1)
+	while (i == 1)
+		if ((i = ft_readfile(fd, i, &buf[fd])) == -1)
 			return (-1);
 	while (buf[fd][i] != '\n' && buf[fd][i] != '\0')
 		i++;
-	if (buf[fd][i] == '\0')
-		count = -1;
 	*line = ft_substr(buf[fd], 0, i);
-	aux = buf[fd];
-	buf[fd] = ft_substr(buf[fd], i + 1, (ft_strlen(buf[fd]) - i));
-	free(aux);
-	if (count == -1)
+	if (buf[fd][i] == '\0')
+	{
+		buf[fd] = ft_free(buf[fd]);
 		return (0);
+	}
+	aux = ft_substr(buf[fd], i + 1, (ft_strlen(buf[fd]) - i));
+	buf[fd] = ft_free(buf[fd]);
+	buf[fd] = aux;
 	return (1);
 }
